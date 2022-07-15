0b2acf45ece14b1a3c5909fb66dfb439a529d1a1
Maciej-Koc <mkoc2@asu.edu>
2022-07-14 23:55:26 -0500

    Completed section 10 step 14 using useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React, { useState } from 'react';
+import React from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+
+  React.useEffect(() => {
+    const storedUserLogged = localStorage.getItem("isLoggedIn");
+    if (storedUserLogged === 1) {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "LOGGED_IN");
     setIsLoggedIn(true);
   };
 
