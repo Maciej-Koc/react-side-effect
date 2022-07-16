aa9dac6c1d33d657725e541274548f49b060cb54
Maciej-Koc <mkoc2@asu.edu>
2022-07-15 22:34:10 -0500

    Created useContext hook for passing login information through Navigation.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./components/Login/store/auth-context";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = React.useState(false);
@@ -27,11 +28,14 @@ function App() {
 
   return (
     <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
+      {/* React Context API */}
+      <AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
+        <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+        <main>
+          {!isLoggedIn && <Login onLogin={loginHandler} />}
+          {isLoggedIn && <Home onLogout={logoutHandler} />}
+        </main>
+      </AuthContext.Provider>
     </React.Fragment>
   );
 }
